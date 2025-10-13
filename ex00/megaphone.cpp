ef258c2235cbec3f60c18ccc76e4a67914025aa6
Yuliano96 <yulianopacileo@gmail.com>
2025-10-13 21:28:57 +0200

    ex00 casi listo

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,44 +6,13 @@
 /*   By: yuliano <yuliano@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 19:06:03 by yuliano           #+#    #+#             */
-/*   Updated: 2025/10/13 00:14:35 by yuliano          ###   ########.fr       */
+/*   Updated: 2025/10/13 21:25:41 by yuliano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-
-/*
-class Megaphone
-{
-	private:
-		const char *str;
-	public:
-		Megaphone(const char *s);
-		const char  *get_mega_phone();
-		~Megaphone();
-};
-
-Megaphone::Megaphone(const char *s) : str(s) {}
-
-const char *Megaphone:: Megaphone::get_mega_phone()
-{
-	return (str);
-}
-
-Megaphone::~Megaphone() {}
-
-int main(int argc, char *argv[])
-{
-	(void)argc;
-	Megaphone p(argv[1]);
-	
-	char *p1 = (char *) p.get_mega_phone();
-	std::cout<<p1<<std::endl;
-	
-	return (0);
-}
-*/
+#include <cctype>
 
 
 class Megaphone
@@ -52,8 +21,9 @@ class Megaphone
 		std::string str;
 	public:
 		Megaphone();
-		void set_mega_phone(const std::string _str);
-		std:: string get_mega_phone();
+		void set_mega_phone(const std::string &xstr);
+		void convert_to_uppercase();
+		std:: string get_mega_phone() const;
 		~Megaphone();
 };
 
@@ -62,40 +32,51 @@ Megaphone::Megaphone()
 	str = "";
 }
 
-std::string Megaphone:: Megaphone::get_mega_phone()
+std::string Megaphone::get_mega_phone() const
 {
 	return (str);
 }
 
-void Megaphone::set_mega_phone(const std::string _str)
+void Megaphone::convert_to_uppercase()
+{
+	for(size_t i = 0; i < str.size(); i++)
+	{
+		str[i] = std::toupper(str[i]);
+	}
+}
+
+void Megaphone::set_mega_phone(const std::string &_str)
 {
 	str = _str;
 }
 
-Megaphone::~Megaphone() {}
+Megaphone::~Megaphone() 
+{
+}
+
 
 int main(int argc, char *argv[])
 {
-	int i;
-
 	if (argc == 1)
-		std::cout<<"Error"<<std::endl;
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
 	else
 	{
-		Megaphone *megaphone = new Megaphone[argc];
-		i = 1;
-		while(i < argc)
-		{
-			megaphone[i].set_mega_phone(argv[i]);
-			i++;
-		}
-		i = 1;
-		while(i < argc)
-		{
-			std::cout<<megaphone[i].get_mega_phone()<<std::endl;
-			i++;
-		}
-		delete [] megaphone;
+		Megaphone *megaphone = new Megaphone[argc - 1];
+        
+        // Llenar los objetos
+        for (int i = 1; i < argc; i++)
+        {
+            megaphone[i - 1].set_mega_phone(argv[i]);
+        }
+        
+        // Convertir y mostrar
+        for (int i = 0; i < (argc - 1); i++)
+        {
+            megaphone[i].convert_to_uppercase();
+            std::cout << megaphone[i].get_mega_phone() <<" ";
+        }
+        std::cout<<std::endl;
+        delete[] megaphone;
 	}
 	
 	return (0);
